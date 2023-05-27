7ea1f08eec8bd0beafa02c37df89d3b585f46ce5
takhahashi <yuu1719m@ezweb.ne.jp>
2023-05-27 16:32:23 +0900

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,11 @@
+
+def main(cfg: DictConfig):
+    cwd = hydra.utils.get_original_cwd()
+    wandb_logger
+class Dataset(Dataset):
+    def __init__(
+            self,
+            df,
+            tokenizer,
+            max_length
+    )

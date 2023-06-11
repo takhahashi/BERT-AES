b468dd6f7f925d521b3ca02f2ad3fbf3cd9be124
takhahashi <yuu1719m@ezweb.ne.jp>
2023-06-11 13:11:32 +0900

    None

--- src/main.py
+++ src/main.py
@@ -42,7 +42,7 @@ def main(cfg: DictConfig):
     wandb_logger = WandbLogger(
         name=cfg.wandb.project_name,
         project=cfg.wandb.project,
-        settings=wandb.Settings(start_method="fork"),
+        settings=wandb.Settings(start_method="thread"),
     )
     checkpoint_path = cfg.path.checkpoint_path
     wandb_logger.log_hyperparams(cfg)

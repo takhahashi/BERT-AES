5e7295d0088823fdc3cdfeb7d4b71ea817cb6c14
takhahashi <125870494+takhahashi@users.noreply.github.com>
2023-06-19 00:07:39 +0900

    None

--- src/reg_train.py
+++ src/reg_train.py
@@ -73,11 +73,11 @@ class Scaler(torch.nn.Module):
 @hydra.main(config_path="/content/drive/MyDrive/GoogleColab/1.AES/ASAP/BERT-AES/configs", config_name="reg_config")
 def main(cfg: DictConfig):
     cwd = hydra.utils.get_original_cwd()
-    wandb.init(name=cfg.wandb.project_name,
-               project=cfg.wandb.project,
-               reinit=True,
-               )
-    
+    wandb.init(name=cfg['wandb']['project_name'],
+            project=cfg['wandb']['project'],
+            reinit=True,
+            )
+
     tokenizer = AutoTokenizer.from_pretrained(cfg.model.model_name_or_path)
     train_dataset = get_Dataset(cfg.model.reg_or_class,
                                 cfg.path.traindata_file_name,
@@ -89,17 +89,17 @@ def main(cfg: DictConfig):
                             cfg.aes.prompt_id,
                             tokenizer,
                             )
-    
+
     train_dataloader = torch.utils.data.DataLoader(train_dataset,
-                                                   batch_size=cfg.training.batch_size,
-                                                   shuffle=True,
-                                                   collate_fn=simple_collate_fn,
-                                                   )
+                                                    batch_size=cfg.training.batch_size,
+                                                    shuffle=True,
+                                                    collate_fn=simple_collate_fn,
+                                                    )
     dev_dataloader = torch.utils.data.DataLoader(dev_dataset,
-                                                   batch_size=cfg.training.batch_size,
-                                                   shuffle=True,
-                                                   collate_fn=simple_collate_fn,
-                                                   )
+                                                    batch_size=cfg.training.batch_size,
+                                                    shuffle=True,
+                                                    collate_fn=simple_collate_fn,
+                                                    )
 
     model = create_module(
         cfg.model.model_name_or_path,
@@ -115,6 +115,7 @@ def main(cfg: DictConfig):
     scaler = torch.cuda.amp.GradScaler()
     sigma_scaler = Scaler(init_S=1.0).cuda()
     for epoch in range(cfg.training.n_epochs):
+        model.current_epoch = epoch
         for idx, t_batch in enumerate(train_dataloader):
             batch = {k: v.cuda() for k, v in t_batch.items()}
             with torch.cuda.amp.autocast():
@@ -128,10 +129,10 @@ def main(cfg: DictConfig):
         model.eval()
         with torch.no_grad():
             dev_results = return_predresults(model, dev_dataloader, rt_clsvec=False, dropout=False)
-        dev_mu = torch.tensor(dev_results['score'])
-        dev_std = torch.tensor(dev_results['logvar'].exp().sqrt())
-        dev_labels = torch.tensor(dev_results['labels'])
-        
+        dev_mu = torch.tensor(dev_results['score']).cuda()
+        dev_std = torch.tensor(dev_results['logvar']).exp().sqrt().cuda()
+        dev_labels = torch.tensor(dev_results['labels']).cuda()
+
         # find optimal S
         s_opt = torch.optim.LBFGS([sigma_scaler.S], lr=3e-2, max_iter=2000)
 
@@ -146,9 +147,9 @@ def main(cfg: DictConfig):
         for idx, d_batch in enumerate(dev_dataloader):
             batch = {k: v.cuda() for k, v in d_batch.items()}
             dev_step_outputs = model.validation_step(batch, idx)
-            dev_mu = dev_step_outputs['score']
+            dev_mu = dev_step_outputs['batch_preds']
             dev_std = dev_step_outputs['logvar'].exp().sqrt()
-            dev_labels = dev_step_outputs['labels']
+            dev_labels = dev_step_outputs['batch_labels']
             loss_all += regvarloss(y_true=dev_labels, y_pre_ave=dev_mu, y_pre_var=sigma_scaler(dev_std).pow(2).log()).to('cpu').detach().numpy().copy()
 
         earlystopping(loss_all, model)

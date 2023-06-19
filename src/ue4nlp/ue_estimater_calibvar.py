e9e2864580e2654671c99a253d8bcaeaaceaf47c
takhahashi <125870494+takhahashi@users.noreply.github.com>
2023-06-19 21:03:22 +0900

    None

--- src/ue4nlp/ue_estimater_calibvar.py
+++ src/ue4nlp/ue_estimater_calibvar.py
@@ -22,7 +22,7 @@ class UeEstimatorCalibvar:
 
         def closure():
             s_opt.zero_grad()
-            loss = regvarloss(y_true=self.dev_labels.cuda(), y_pre_ave=self.dev_score.cuda(), y_pre_var=self.sigma_scaler(dev_std).pow(2).log().cuda())
+            loss = regvarloss(y_true=self.dev_labels.cuda(), y_pre_ave=self.dev_score.cuda(), y_pre_var=self.sigma_scaler(dev_std).pow(2).log())
             loss.backward()
             return loss
         s_opt.step(closure)

fa38cf170bbac2546749371d175cc07b92795aaf
takhahashi <125870494+takhahashi@users.noreply.github.com>
2023-07-01 14:08:27 +0900

    None

--- src/reg_class_mix_train.py
+++ src/reg_class_mix_train.py
@@ -18,7 +18,7 @@ from models.models import Reg_class_mixmodel
 from utils.cfunctions import regvarloss, EarlyStopping
 from models.models import Scaler
 
-@hydra.main(config_path="/content/drive/MyDrive/GoogleColab/1.AES/ASAP 2/BERT-AES/configs", config_name="reg_class_mix")
+@hydra.main(config_path="/content/drive/MyDrive/GoogleColab/1.AES/ASAP/BERT-AES/configs", config_name="reg_class_mix")
 def main(cfg: DictConfig):
     tokenizer = AutoTokenizer.from_pretrained(cfg.model.model_name_or_path)
     train_dataset = get_Dataset('reg',

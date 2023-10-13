bc600d8072d047518c1ed54bb093e78f8d97f0fc
takhahashi <yuu1719m@ezweb.ne.jp>
2023-10-13 13:28:18 +0900

    None

--- src/eval_accuracy.py
+++ src/eval_accuracy.py
@@ -154,7 +154,25 @@ def main(cfg: DictConfig):
     with open(save_path, mode="wt", encoding="utf-8") as f:
         json.dump(results_dic, f, ensure_ascii=False)
 
-
+    ##GP###
+    five_fold_results = []
+    for fold in range(5):
+        with open('/content/drive/MyDrive/GoogleColab/1.AES/ASAP/GP-git/pt${}/fold_${}/pred_results'.format(cfg.aes.prompt_id, fold)) as f:
+            fold_results = json.load(f)
+        five_fold_results.append({k: np.array(v) for k, v in fold_results.items()})
+    corr_arr, qwk_arr, rmse_arr = [], [], []
+    for foldr in five_fold_results:
+        true = foldr['labels'].astype('int32')
+        pred = foldr['score']
+        corr_arr = np.append(corr_arr, np.corrcoef(true, pred)[0][1])
+        qwk_arr = np.append(qwk_arr, calc_qwk(true, pred, prompt_id, 'class'))
+        rmse_arr = np.append(rmse_arr, np.sqrt((true - pred) ** 2).mean())
+    results_dic = {'qwk': np.mean(qwk_arr), 
+                    'corr': np.mean(corr_arr), 
+                    'rmse': np.mean(rmse_arr)}
+    save_path = save_dir_path + '/GP'
+    with open(save_path, mode="wt", encoding="utf-8") as f:
+        json.dump(results_dic, f, ensure_ascii=False)
     """
     five_fold_results = []
     for fold in range(5):

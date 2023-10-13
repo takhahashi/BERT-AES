a3bb30b7a1c0dc2a74538957eb549387bcf5e5a2
takhahashi <yuu1719m@ezweb.ne.jp>
2023-10-13 13:50:17 +0900

    None

--- src/eval_accuracy.py
+++ src/eval_accuracy.py
@@ -15,7 +15,7 @@ from transformers import AutoTokenizer
 from utils.utils_data import TrainDataModule
 from utils.cfunctions import simple_collate_fn, score_f2int
 from utils.utils_models import create_module
-from utils.dataset import get_Dataset
+from utils.dataset import get_Dataset, get_score_range
 from utils.ue_metric_func import calc_rcc_auc, calc_rpp, calc_roc_auc, calc_risk
 from utils.acc_metric_func import calc_qwk
 from models.functions import return_predresults
@@ -29,6 +29,7 @@ from sklearn.metrics import cohen_kappa_score
 def main(cfg: DictConfig):
     save_dir_path = cfg.path.save_dir_path
     prompt_id = cfg.aes.prompt_id
+    low, high = get_score_range(prompt_id)
 
     five_fold_results = []
     for fold in range(5):
@@ -104,8 +105,8 @@ def main(cfg: DictConfig):
     corr_arr, qwk_arr, rmse_arr = [], [], []
     ##simple_class####
     for foldr in five_fold_results:
-        true = foldr['labels'].astype('int32')
-        pred = np.argmax(foldr['logits'], axis=-1).astype('int32')
+        true = foldr['labels'].astype('int32') + low
+        pred = np.argmax(foldr['logits'], axis=-1).astype('int32') + low
 
         corr_arr = np.append(corr_arr, np.corrcoef(true, pred)[0][1])
         qwk_arr = np.append(qwk_arr, calc_qwk(true, pred, prompt_id, 'class'))
@@ -122,8 +123,8 @@ def main(cfg: DictConfig):
     corr_arr, qwk_arr, rmse_arr = [], [], []
     ##dp_class####
     for foldr in five_fold_results:
-        true = foldr['labels'].astype('int32')
-        pred = foldr['mcdp_score'].astype('int32')
+        true = foldr['labels'].astype('int32') + low
+        pred = foldr['mcdp_score'].astype('int32') + low
 
         corr_arr = np.append(corr_arr, np.corrcoef(true, pred)[0][1])
         qwk_arr = np.append(qwk_arr, calc_qwk(true, pred, prompt_id, 'class'))
@@ -140,8 +141,8 @@ def main(cfg: DictConfig):
     corr_arr, qwk_arr, rmse_arr = [], [], []
     ##ense_class####
     for foldr in five_fold_results:
-        true = foldr['labels'].astype('int32')
-        pred = foldr['ense_score'].astype('int32')
+        true = foldr['labels'].astype('int32') + low
+        pred = foldr['ense_score'].astype('int32') + low
 
         corr_arr = np.append(corr_arr, np.corrcoef(true, pred)[0][1])
         qwk_arr = np.append(qwk_arr, calc_qwk(true, pred, prompt_id, 'class'))
@@ -157,13 +158,13 @@ def main(cfg: DictConfig):
     ##GP###
     five_fold_results = []
     for fold in range(5):
-        with open('/content/drive/MyDrive/GoogleColab/1.AES/ASAP/GP-git/pt${}/fold_${}/pred_results'.format(cfg.aes.prompt_id, fold)) as f:
+        with open('/content/drive/MyDrive/GoogleColab/1.AES/ASAP/GP-git/pt{}/fold_{}/pred_results'.format(cfg.aes.prompt_id, fold)) as f:
             fold_results = json.load(f)
         five_fold_results.append({k: np.array(v) for k, v in fold_results.items()})
     corr_arr, qwk_arr, rmse_arr = [], [], []
     for foldr in five_fold_results:
-        true = foldr['labels'].astype('int32')
-        pred = foldr['score']
+        true = foldr['labels'].astype('int32') + low
+        pred = np.round(foldr['score'] + low).astype('int32')
         corr_arr = np.append(corr_arr, np.corrcoef(true, pred)[0][1])
         qwk_arr = np.append(qwk_arr, calc_qwk(true, pred, prompt_id, 'class'))
         rmse_arr = np.append(rmse_arr, np.sqrt((true - pred) ** 2).mean())
--- src/utils/acc_metric_func.py
+++ src/utils/acc_metric_func.py
@@ -6,4 +6,4 @@ def calc_qwk(true, pred, prompt_id, reg_or_class):
     if reg_or_class == 'reg':
         return cohen_kappa_score(true, pred, labels = list(range(low, high + 1)), weights='quadratic')
     elif reg_or_class == 'class':
-        return cohen_kappa_score(true, pred, labels = list(range(high - low + 1)), weights='quadratic')
+        return cohen_kappa_score(true, pred, labels = list(range(low, high + 1)), weights='quadratic')

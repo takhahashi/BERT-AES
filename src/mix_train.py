b32609fdf1e9f4910f5f21e2ffaf05829a862037
takhahashi <yuu1719m@ezweb.ne.jp>
2024-01-04 02:37:54 +0900

    NOne

--- src/mix_train.py
+++ src/mix_train.py
@@ -23,8 +23,8 @@ import wandb
 @hydra.main(config_path="/content/drive/MyDrive/GoogleColab/1.AES/ASAP/BERT-AES/configs", config_name="reg_class_mix")
 def main(cfg: DictConfig):
 
-    wandb.init(project=cfg.wandb.project, 
-               name=cfg.wandb.project_name,)
+    #wandb.init(project=cfg.wandb.project, 
+    #           name=cfg.wandb.project_name,)
                #settings=wandb.Settings(start_method="thread"))
 
     
@@ -69,7 +69,7 @@ def main(cfg: DictConfig):
 
     trainloss_list, devloss_list, dev_mse_list, dev_cross_list = [], [], [], []
     scaler = torch.cuda.amp.GradScaler()
-
+    """
     lossall, mse_lossall = 0, 0
     for data in train_dataloader:
         data = {k: v.cuda() for k, v in data.items()}
@@ -78,7 +78,8 @@ def main(cfg: DictConfig):
 
         lossall += loss.to('cpu').detach().numpy().copy()
         mse_lossall += mse_loss.to('cpu').detach().numpy().copy()
-    mse_weights = lossall / mse_lossall
+    """
+    mse_weights = 1.
 
     for epoch in range(cfg.training.n_epochs):
         model.train()
@@ -118,7 +119,7 @@ def main(cfg: DictConfig):
             loss, mse_loss, cross_loss = mix_loss1(d_data['labels'].to('cpu').detach().squeeze(), dev_outputs['score'].squeeze(), dev_outputs['logits'], high, low, alpha=mse_weights)
             devlossall += loss.to('cpu').detach().numpy().copy()
 
-        wandb.log({"epoch":epoch+0.001,"all_loss":lossall, "mse_loss":mse_lossall, "cross_loss":cross_lossall,"dev_loss":devlossall,"mse_weights":mse_weights})
+        #wandb.log({"epoch":epoch+0.001,"all_loss":lossall, "mse_loss":mse_lossall, "cross_loss":cross_lossall,"dev_loss":devlossall,"mse_weights":mse_weights})
         devloss_list = np.append(devloss_list, devlossall/num_dev_batch)
         #dev_mse_list = np.append(dev_mse_list, mseloss_el)
         #dev_cross_list = np.append(dev_cross_list, crossentropy_el)
@@ -142,7 +143,7 @@ def main(cfg: DictConfig):
         print(f'Epoch:{epoch}, train_Loss:{lossall/num_train_batch:.4f}, dev_loss:{devlossall/num_dev_batch:.4f}')
         earlystopping(devlossall/num_dev_batch, model)
         if(earlystopping.early_stop == True): break
-    wandb.finish()
+    #wandb.finish()
     """
     # Plot trainloss_list in blue
     plt.plot(trainloss_list, color='blue', label='Train Loss')

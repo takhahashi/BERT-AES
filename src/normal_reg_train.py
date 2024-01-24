0a98a84b9c78852743241a2f0ae9b440b6720fe3
takhahashi <yuu1719m@ezweb.ne.jp>
2024-01-24 20:43:39 +0900

    NOne

--- src/normal_reg_train.py
+++ src/normal_reg_train.py
@@ -98,9 +98,8 @@ def main(cfg: DictConfig):
         model.eval()
         for idx, d_batch in enumerate(dev_dataloader):
             batch = {k: v.cuda() for k, v in d_batch.items()}
-            dev_score = model(batch)['score'].to('cpu').detach().numpy().copy()
-            print(batch['labels'].to('cpu').detach().numpy().copy())
-            dev_loss = mseloss(dev_score.squeeze(), batch['labels'].to('cpu').detach().numpy().copy())
+            dev_score = model(batch)['score'].to('cpu').detach().squeeze().numpy().copy()
+            dev_loss = mseloss(dev_score, batch['labels'].to('cpu').detach().numpy().copy())
             dev_loss_all += dev_loss
 
         print(f'Epoch:{epoch}, train_loss:{train_loss_all/num_train_batch}, dev_loss:{dev_loss_all/num_dev_batch}')
